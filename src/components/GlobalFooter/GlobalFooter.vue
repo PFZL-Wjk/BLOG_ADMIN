1056dcec9543493348c1bb99e811f092bacb2087
DESKTOP-4950SS1\jkwgq <1046098651@qq.com>
2020-12-05 18:48:44 +0800

    initialize

---
+++ src/components/GlobalFooter/GlobalFooter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="footer">
+    <!-- <div class="links">
+      <a
+        href="http://helloblog.byteblogs.com/#/"
+        target="_blank"
+      >hello blog 首页</a>
+      <a
+        href="https://github.com/byteblogs168/hello-blog-admin"
+        target="_blank"
+      >
+        <a-icon type="github" />
+      </a>
+      <a href="http://helloblog.byteblogs.com/docs/">hello blog</a>
+      <a href="https://www.byteblogs.com/">byteblogs</a>
+    </div> -->
+    <div class="copyright">
+      <span> 🐟你一起</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooter',
+  data () {
+    return {
+      year: new Date().getFullYear()
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.footer {
+  padding: 0 16px;
+  margin: 48px 0 24px;
+  text-align: center;
+
+  .links {
+    margin-bottom: 8px;
+
+    a {
+      color: rgba(0, 0, 0, 0.45);
+
+      &:hover {
+        color: rgba(0, 0, 0, 0.65);
+      }
+
+      &:not(:last-child) {
+        margin-right: 40px;
+      }
+    }
+  }
+  .copyright {
+    color: rgba(0, 0, 0, 0.45);
+    font-size: 14px;
+  }
+}
+</style>
